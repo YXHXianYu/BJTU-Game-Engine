8865abab73f07ff62cd36777b02b88174f16d20c
YXHXianYu <2943003@qq.com>
2024-04-15 13:09:55 +0800

    chore: add some dirs to gitignore

--- cmake-build-debug/engine/3rdparty/assimp-5.3.1/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Documents/BJTU-Game-Engine/engine/3rdparty/assimp-5.3.1/contrib/zlib
-# Build directory: D:/Documents/BJTU-Game-Engine/cmake-build-debug/engine/3rdparty/assimp-5.3.1/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
